51916306c67846e1055b44b77ce683e1415cac0c
AnhTuanMaiZuN <anhtuanmai0709@gmail.com>
2023-06-10 15:25:24 +0700

    commint buoi 3

---
+++ Buoi_3/buoi_3.py
@@ -0,0 +1,41 @@
+a = 10
+b = "string"
+print("a =", a)
+print("b=", b)
+print("a + b =", a, b)
+print("a + b =", str(a) + b, sep = " ")
+
+# comment
+# handname ps: ZuN
+'''
+Ten = input("Tên của bạn: ")
+Ho_dem = input("Họ và tên đệm của bạn: ")
+Tuoi = input("Tuỏi của bạn là bao nhiêu: ")
+
+print("Tên đầy đủ của bạn là: ", Ho_dem, Ten)
+print("Tuổi hiện tại là:", Tuoi)
+'''
+'''
+GT = input("Giới tính của bạn: ")
+ST = input("Sở thích ăn uống của bạn: ")
+DA = input("Đồ ăn bị dị ứng: ")
+
+print("Giới tính của bạn là: ", GT)
+print("Sở thích ăn uống của bạn là: ", ST)
+print("Đồ ăn bị dị ứng là: ", DA)
+'''
+'''
+a = float(input("a= "))
+b = float(input("b= "))
+c = float(input("c= "))
+
+print("(a + b + c)/3= ", (a + b + c) / 3)
+'''
+a = float("35205000")
+b = float("1500")
+
+print("1 đô la bằng= ", a / b)
+
+d = float("1257125")
+
+print(a // d + 1)
