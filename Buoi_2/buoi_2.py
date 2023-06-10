464c867e4fffcf221ccf5b8bff3ddcc9c44d71be
AnhTuanMaiZuN <anhtuanmai0709@gmail.com>
2023-06-10 13:37:09 +0700

    commit buoi 2

---
+++ Buoi_2/buoi_2.py
@@ -0,0 +1,22 @@
+x = 10
+y = 5
+print(x + y)
+a = 10
+b = 24
+c = 2
+print(a * b / c)
+i1 = 2
+i2 = 5
+i3 = -3
+print ("a) i1 + (i2 * i3) =", i1 + (i2 * i3))
+print ("b) i1 * (i2 + i3) =", i1 * (i2 + i3))
+print ("c) i1 / (i2 + i3) =", i1 / (i2 + i3))
+print ("d) i1 // (i2 + i3) =", i1 // (i2 + i3))
+ten = "mai anh tuan"
+lop = "8A5"
+tuoi = 13
+diem = 10
+print (ten)
+print (lop)
+print (tuoi)
+print (diem)
