45d6f6a04b0c1450af1c0281a3d90d3dae90bf2c
AnhTuanMaiZuN <anhtuanmai0709@gmail.com>
2023-06-16 14:29:16 +0700

    commit baitap

---
+++ Buoi_3/baitapvenha.py
@@ -0,0 +1,17 @@
+'''
+b = input("Số tiền vnđ là")
+a = float(23,527,50)
+print("Quy đổi sang USD ta được:" (b*a))
+'''
+'''
+c = float(input("Bán kính của hình tròn là:"))
+d = float("3.14")
+e = 2
+print("Chu vi hình tròn là:", e * c * d)
+print("Diện tích hình tròn là:", d * (c^e))
+'''
+f = input("Thời gian về đích vận động viên thứ 1 là:")
+g = input("Thời gian về đích vận động viên thứ 2 là:")
+h = input("Thời gian về đích vận động viên thứ 3 là:")
+i = 3
+print("thời gian trung bình các vận động viên về đích là", (f + g + h) / i)
