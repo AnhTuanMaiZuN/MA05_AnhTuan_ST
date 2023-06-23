575ff8a5e55b460e9b7dbec48f1dfcbc9688a545
AnhTuanMaiZuN <anhtuanmai0709@gmail.com>
2023-06-23 15:38:05 +0700

    commit baitapvenha buoi 4

---
+++ Buoi_4/baitapvenha.py
@@ -0,0 +1,31 @@
+'''
+n = int(input("Hãy nhập một số nguyên: "))
+if (n % 2 == 0):
+    print(n, "là số chẵn")
+else:
+    print(n, "là số lẻ")
+'''
+'''
+print("1.Điểm này thuộc loại giỏi")
+print("2.Điểm này thuộc loại khá")
+print("3.Điểm này thuộc loại trung bình")
+print("4.Điểm này thuộc loại yếu")
+
+diem = int(input("Điểm trung bình là: "))
+if (diem == 9):
+    print("Điểm này thuộc loại giỏi")
+elif (diem >= 7):
+    print("Điểm này thuộc loại khá")
+elif (diem >= 5):
+    print("Điểm này thuộc loại trung bình")
+elif (diem <= 5):
+    print("Điểm này thuộc loại yếu")
+'''
+'''
+nam = int(input("Năm này là năm: "))
+if (nam % 4 == 0):
+    print(nam, "là năm nhuận")
+else:
+    print(nam, "là năm không nhuận")
+'''
+
